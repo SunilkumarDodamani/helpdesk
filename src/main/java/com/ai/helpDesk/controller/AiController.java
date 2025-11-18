d3aa8a3d1b869159d9a26e1927935dcfd9862833
sunilkumar <sunilkumardodamani05@gmail.com>
2025-11-18 12:00:40 +0530

    cors

--- src/main/java/com/ai/helpDesk/controller/AiController.java
+++ src/main/java/com/ai/helpDesk/controller/AiController.java
@@ -4,7 +4,7 @@ import com.ai.helpDesk.Service.AIService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "https://691c05b7c1411bb4bc9c372f--sunil-helpdesk-ai.netlify.app")
+@CrossOrigin(origins = "https://sunil-helpdesk-ai.netlify.app")
 @RestController
 @RequestMapping("/api/v1/ai")
 public class AiController {
